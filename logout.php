ff295332fe9c22024ef7e06b591bcd1e9f63d8f1
reddevils9h <32790979+reddevils9h@users.noreply.github.com>
2017-11-12 14:17:30 +0700

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start();
-	if(isset($_SESSION['fullName']) && isset($_SESSION['username']) && isset($_SESSION['userId'])){
-		unset($_SESSION['fullName']);
-		unset($_SESSION['username']);
-		unset($_SESSION['userId']);
-	}
-	header("location: ../btl.php");
-?>
