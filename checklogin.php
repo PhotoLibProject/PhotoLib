f8ec0dd36c96a3e19b647e27368c01fe8cababaf
reddevils9h <32790979+reddevils9h@users.noreply.github.com>
2017-11-12 14:16:52 +0700

    member
    member

---
+++ checklogin.php
@@ -0,0 +1,25 @@
+<?php
+	session_start();
+	include '../connect.php';
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+
+	$username = stripslashes($username);
+	$password = stripcslashes($password);
+	$username = mysql_real_escape_string($username);
+	$password = mysql_real_escape_string($password);
+
+	$sql = "SELECT * FROM user WHERE username = '$username' AND password = '$password'";
+	$result = mysql_query($sql);
+	$row = mysql_fetch_array($result);
+	$count = mysql_num_rows($result);
+	if($count != 0){
+		$_SESSION['fullName'] = $row['fullName'];
+		$_SESSION['username'] = $username;
+		$_SESSION['userId'] = $row['userId'];
+		header("location: ../BTL.php");
+	}
+	else{
+		header("location: login.php");
+	}
+?>
